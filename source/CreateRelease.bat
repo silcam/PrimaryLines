84a4e65de308cb601b32f63ccc01b70188d655d2
JPRCam <33322570+JPRCam@users.noreply.github.com>
2018-03-13 16:55:32 +0100

    ignoring files

--- source/CreateRelease.bat
+++ source/CreateRelease.bat
@@ -5,16 +5,16 @@ SET /P AREYOUSURE=Have you changed the version number and exported both .ttf and
 IF /I "%AREYOUSURE%" == "E" GOTO EXPORT
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 set /P ver=Enter Version: 
-mkdir "..\release\PrimaryLines %ver%"
-copy "..\FONTLOG-PrimaryLines.txt" "..\release\PrimaryLines %ver%\"
-copy "..\OFL-PrimaryLines.txt" "..\release\PrimaryLines %ver%\"
-copy "..\ReadMe.txt" "..\release\PrimaryLines %ver%\"
-copy "Andika" "..\release\PrimaryLines %ver%\"
-copy "*.ttf" "..\release\PrimaryLines %ver%\"
-copy "*.otf" "..\release\PrimaryLines %ver%\"
-copy "*.woff" "..\release\PrimaryLines %ver%\"
-CScript  zip.vbs  "..\release\PrimaryLines %ver%"  "..\release\PrimaryLines Current.zip
-CScript  zip.vbs  "..\release\PrimaryLines %ver%"  "..\release\PrimaryLines %ver%.zip
+mkdir "..\release\PrimaryLines-%ver%"
+copy "..\FONTLOG.txt" "..\release\PrimaryLines-%ver%\"
+copy "..\OFL.txt" "..\release\PrimaryLines-%ver%\"
+copy "..\ReadMe.txt" "..\release\PrimaryLines-%ver%\"
+copy "*.ttf" "..\release\PrimaryLines-%ver%\"
+copy "*.otf" "..\release\PrimaryLines-%ver%\"
+mkdir "..\release\PrimaryLines-%ver%\web\"
+copy "*.woff" "..\release\PrimaryLines-%ver%\web\"
+CScript  zip.vbs  "..\release\PrimaryLines-%ver%"  "..\release\PrimaryLines Current.zip
+CScript  zip.vbs  "..\release\PrimaryLines-%ver%"  "..\release\PrimaryLines-%ver%.zip
 GOTO END
 
 :EXPORT
